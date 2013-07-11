5fc672994ae36e4a64f997b4c7e9e87ca779529f
Walter Bender <walter@sugarlabs.org>
2013-07-11 16:21:32 -0400

    new repo

---
+++ js/activity.js
@@ -0,0 +1,25 @@
+define(function (require) {
+    var activity = require("sugar-web/activity/activity");
+    var icon = require("sugar-web/graphics/icon");
+
+    // Manipulate the DOM only when it is ready.
+    require(['domReady!'], function (doc) {
+
+        // Initialize the activity.
+        activity.setup();
+
+        // Colorize the activity icon.
+        var activityButton = document.getElementById("activity-button");
+        activity.getXOColor(function (error, colors) {
+            icon.colorize(activityButton, colors);
+        });
+
+        // Make the activity stop with the stop button.
+        var stopButton = document.getElementById("stop-button");
+        stopButton.addEventListener('click', function (e) {
+            activity.close();
+        });
+
+    });
+
+});

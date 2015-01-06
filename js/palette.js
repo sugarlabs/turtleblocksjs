acb04f76d86ce28a3e74d4bda41decd454b2d3cb
Ignacio Rodríguez <ignacio@sugarlabs.org>
2015-01-06 04:14:57 -0200

    Little tour in first startup

--- js/palette.js
+++ js/palette.js
@@ -635,9 +635,15 @@ function initPalettes(canvas, stage, cellSize, refreshCanvas) {
     add('extras', 'white', '#ff0066').
     add('mashape', 'white', '#ff0066');
 
+
     // Give the palettes time to load.
     setTimeout(function() {
         palettes.show();
+        cookie = getCookie("turtlejstour");
+        if (!cookie) {
+            palettes.dict['turtle'].show();
+            palettes.dict['turtle'].showMenuItems();
+        }
         palettes.bringToTop();
     }, 2000);
     return palettes;
@@ -823,4 +829,15 @@ function last(myList) {
     } else {
         return myList[i - 1];
     }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
 }

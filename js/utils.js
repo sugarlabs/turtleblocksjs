3fd6be0330efd827c3e3767d001e6a7bd502b828
Sam Parkinson <sam.parkinson3@gmail.com>
2015-02-13 21:31:01 +1100

    Fix a few bugs with the tour
    * Don't re-rotate the menu icon if it is already down
    * Use localstorage, cordova doesn't do cookies

--- js/utils.js
+++ js/utils.js
@@ -136,22 +136,6 @@ function fileBasename(file) {
 }
 
 
-function getCookie(cname) {
-    var name = cname + '=';
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return '';
-}
-
-
 // Needed to generate new data for localization.ini
 // var translated = "";
 function _(text) {

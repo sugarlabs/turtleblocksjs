981ab6b6f3cdeb65965c05c0a3f065a154cf4f7d
Walter Bender <walter@sugarlabs.org>
2015-10-13 13:26:47 -0400

    update license to AGPL

--- js/samplesviewer.js
+++ js/samplesviewer.js
@@ -1,19 +1,14 @@
-/*
-Copyright (C) 2015 Sam Parkinson
+// Copyright (C) 2015 Sam Parkinson
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the The GNU Affero General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+//
 
 APIKEY = '3tgTzMXbbw6xEKX7';
 EMPTYIMAGE = 'data:image/svg+xml;base64,' + btoa('<svg \

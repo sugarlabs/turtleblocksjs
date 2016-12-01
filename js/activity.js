f79e7ad9473549ad9ad36d07109fc73e6bcd6229
Samson Goddy <samswag@users.noreply.github.com>
2016-12-01 08:39:51 +0100

    updated it v2.8

--- js/activity.js
+++ js/activity.js
@@ -20,7 +20,7 @@ function facebookInit() {
         FB.init({
             appId: '1496189893985945',
             xfbml: true,
-            version: 'v2.1'
+            version: 'v2.8'
         });
 
         // ADD ADDITIONAL FACEBOOK CODE HERE
